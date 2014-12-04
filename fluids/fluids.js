b7ee787f92bb15af8c562e22785ffe05b37b5de8
Santiago J <santi256@gmail.com>
2014-12-04 01:40:33 -0600

    pretty much final commit

--- fluids/fluids.js
+++ fluids/fluids.js
@@ -1,4 +1,4 @@
-(function() {
+(function(exports) {
 
 "use strict";
 
@@ -10,18 +10,19 @@ function xy2i(x, y) { return sim.N*y + x; }
 var sim = {
     N: 1,
     size: 1,
-    FloatArray: Float64Array,
-    u: null,
-    v: null,
     u0: null,
+    u1: null,
+    ubnd: null,
     v0: null,
-    q: null,
+    v1: null,
+    vbnd: null,
     q0: null,
-    bnd: null,
+    q1: null,
+    qbnd: null,
     zeros: null,
-    viscosity: 1e-2,
+    viscosity: 1e-3,
     diffusion: 1e-3,
-    num_iter: 10,
+    numIter: 10,
     canvas: null,
     ctx: null,
     animationFrameId: 0,
@@ -37,140 +38,168 @@ var sim = {
         v: 0,
         active: false,
         type: 0,
+    },
+    forces: [],
+    forceXEl: null,
+    forceYEl: null,
+    bndType: 0,
+};
+
+sim.resetBoundaries = function() {
+    var N = sim.N;
+
+    for (var i=0; i<sim.ubnd.length; i++) {
+        sim.ubnd[i] = -1;
+        sim.vbnd[i] = -1;
+        sim.qbnd[i] = -1;
+    }
+
+    switch (sim.bndType) {
+    case 1:
+        for (var i=0; i<N; i++) {
+            sim.vbnd[xy2i(i,  0)] = 0;  // top
+            sim.vbnd[xy2i(i,N-1)] = 0;  // bottom
+            sim.ubnd[xy2i(  0,i)] = 6;  // left
+            sim.ubnd[xy2i(N-1,i)] = 4;  // right
+            sim.qbnd[xy2i(i,  0)] = 2;  // top
+            sim.qbnd[xy2i(i,N-1)] = 8;  // bottom
+            sim.qbnd[xy2i(  0,i)] = 6;  // left
+            sim.qbnd[xy2i(N-1,i)] = 4;  // right
+        }
+        break;
+    case 0:
+    default:
+        for (var i=0; i<N; i++) {
+            sim.vbnd[xy2i(i,  0)] = 0;  // top
+            sim.vbnd[xy2i(i,N-1)] = 0;  // bottom
+            sim.ubnd[xy2i(  0,i)] = 0;  // left
+            sim.ubnd[xy2i(N-1,i)] = 0;  // right
+            sim.qbnd[xy2i(i,  0)] = 2;  // top
+            sim.qbnd[xy2i(i,N-1)] = 8;  // bottom
+            sim.qbnd[xy2i(  0,i)] = 6;  // left
+            sim.qbnd[xy2i(N-1,i)] = 4;  // right
+        }
     }
 };
 
-sim.resetBoundary = function() {
-    var bnd = sim.bnd;
-    bnd.set(sim.zeros);
+sim.gaussSeidelLaplacian = function(q1, q0, qbnd, k, a, numIter) {
+    var kinv = 1/k;
     var N = sim.N;
-    for (var i=0; i<N; i++) {
-        bnd[xy2i(i,0)] = bnd[xy2i(i,N-1)] = bnd[xy2i(0,i)] = bnd[xy2i(N-1,i)] = 1;
-    }
-}
 
-sim.addSource = function(q, q0, dt) {
-    for (var i=0; i < q.length; i++) {
-        q[i] += q0[i] * dt;
-    }
-}
-
-sim.diffuse = function(N, bnd, q, q0, rate, dt) {
-    var a = q.length*rate*dt;
-    var denom = 1/(1+4*a);
-
-    for (var k=0; k<sim.num_iter; k++) {
+    for (var k=0; k<numIter; k++) {
         for (var j=1; j<N-1; j++) {
             for (var i=1; i<N-1; i++) {
-                q[xy2i(i,j)] = denom*(q0[xy2i(i,j)] +
-                             a*(q[xy2i(i-1,j)] + q[xy2i(i+1,j)] +
-                                q[xy2i(i,j-1)] + q[xy2i(i,j+1)]));
+                q1[xy2i(i,j)] = kinv*(q0[xy2i(i,j)] +
+                                   a*(q1[xy2i(i-1,j)] + q1[xy2i(i+1,j)] +
+                                      q1[xy2i(i,j-1)] + q1[xy2i(i,j+1)]));
             }
         }
-        setBound(N, bnd, q);
+        sim.setBound(q1, qbnd);
     }
-}
+};
 
-sim.advect = function(N, bnd, q, q0, u, v, dt) {
+sim.addSource = function(q1, q0, dt) {
+    for (var i=0; i < q1.length; i++) {
+        q1[i] += q0[i] * dt;
+    }
+};
+
+sim.diffuse = function(q1, q0, qbnd, rate, dt) {
+    var a = q1.length*rate*dt;
+    sim.gaussSeidelLaplacian(q1, q0, qbnd, 4*a + 1, a, sim.numIter);
+};
+
+sim.advect = function(q1, q0, u, v, qbnd, dt) {
+    var N = sim.N;
+
     for (var j=1; j<N-1; j++) {
         for (var i=1; i<N-1; i++) {
             var ind = xy2i(i,j);
             var x = i - dt*N * u[ind];
             var y = j - dt*N * v[ind];
-            x = clamp(x,0,N-1.001);
-            y = clamp(y,0,N-1.001);
+            x = clamp(x,0.5,N-1.5);
+            y = clamp(y,0.5,N-1.5);
             var i0 = x|0, i1 = i0 + 1;
             var j0 = y|0, j1 = j0 + 1;
-            q[ind] = q0[xy2i(i0,j0)]*(i1-x)*(j1-y)
-                   + q0[xy2i(i1,j0)]*(x-i0)*(j1-y)
-                   + q0[xy2i(i0,j1)]*(i1-x)*(y-j0)
-                   + q0[xy2i(i1,j1)]*(x-i0)*(y-j0);
+            q1[ind] = q0[xy2i(i0,j0)]*(i1-x)*(j1-y)
+                    + q0[xy2i(i1,j0)]*(x-i0)*(j1-y)
+                    + q0[xy2i(i0,j1)]*(i1-x)*(y-j0)
+                    + q0[xy2i(i1,j1)]*(x-i0)*(y-j0);
         }
     }
-    setBound(N, bnd, x);
-}
+    sim.setBound(q1, qbnd);
+};
 
-sim.project = function(N, u, v, p, div) {
+sim.project = function(u, v, p, div) {
+    var N = sim.N;
     var h = 1/N;
 
+    p.set(sim.zeros);
+
+    // Get divergence of u
     for (var j=1; j<N-1; j++) {
         for (var i=1; i<N-1; i++) {
             div[xy2i(i,j)] = -0.5*h*(u[xy2i(i+1,j)] - u[xy2i(i-1,j)] +
                                      v[xy2i(i,j+1)] - v[xy2i(i,j-1)]);
-            p[xy2i(i,j)] = 0;
         }
     }
-    setBound(N, 0, div);
-    setBound(N, 0, p);
+    sim.setBound(div, sim.qbnd);
 
-    for (var k=0; k<sim.num_iter; k++) {
-        for (var j=1; j<N-1; j++) {
-            for (var i=1; i<N-1; i++) {
-                p[xy2i(i,j)] = 0.25*(div[xy2i(i,j)] +
-                p[xy2i(i-1,j)] + p[xy2i(i+1,j)] +
-                p[xy2i(i,j-1)] + p[xy2i(i,j+1)]);
-            }
-        }
-        setBound(N, 0, p);
-    }
+    // Solve for p
+    sim.gaussSeidelLaplacian(p, div, sim.qbnd, 4, 1, sim.numIter);
 
+    // Subtract grad(p)
     for (var j=1; j<N-1; j++) {
         for (var i=1; i<N-1; i++) {
-            u[xy2i(i,j)] -= 0.5*N*(p[xy2i(i+1,j)] - p[xy2i(i-1,j)]);
-            v[xy2i(i,j)] -= 0.5*N*(p[xy2i(i,j+1)] - p[xy2i(i,j-1)]);
+            u[xy2i(i,j)] -= 0.5/h*(p[xy2i(i+1,j)] - p[xy2i(i-1,j)]);
+            v[xy2i(i,j)] -= 0.5/h*(p[xy2i(i,j+1)] - p[xy2i(i,j-1)]);
         }
     }
-    setBound(N, 1, u);
-    setBound(N, 2, v);
-}
 
-function setBound(N, b, x) {
-    // for (var i=1; i<=N; i++) {
-    //     x[xy2i(  0,  i)] = b==1 ? -x[xy2i(1,i)] : x[xy2i(1,i)];
-    //     x[xy2i(N+1,  i)] = b==1 ? -x[xy2i(N,i)] : x[xy2i(N,i)];
-    //     x[xy2i(  i,  0)] = b==2 ? -x[xy2i(i,1)] : x[xy2i(i,1)];
-    //     x[xy2i(  i,N+1)] = b==2 ? -x[xy2i(i,N)] : x[xy2i(i,N)];
-    // }
-    // x[xy2i(  0,  0)] = 0.5*(x[xy2i(1,  0)] + x[xy2i(  0,1)]);
-    // x[xy2i(  0,N+1)] = 0.5*(x[xy2i(1,N+1)] + x[xy2i(  0,N)]);
-    // x[xy2i(N+1,  0)] = 0.5*(x[xy2i(N,  0)] + x[xy2i(N+1,1)]);
-    // x[xy2i(N+1,N+1)] = 0.5*(x[xy2i(N,N+1)] + x[xy2i(N+1,N)]);
+    sim.setBound(u,sim.ubnd);
+    sim.setBound(v,sim.vbnd);
+};
 
-    for (var i=0; i<x.length; i++) {
-        if (sim.bnd[i] > 0) {
-            x[i] = 0;
+sim.setBound = function(q, qbnd) {
+    var N = sim.N;
+
+    for (var i=0; i<q.length; i++) {
+        switch (qbnd[i]) {
+        case 0:  // zero
+            q[i] = 0;
+            break;
+        case 2:  // top side
+            q[i] = q[i+N];
+            break;
+        case 4:  // right side
+            q[i] = q[i-1];
+            break;
+        case 6:  // left side
+            q[i] = q[i+1];
+            break;
+        case 8:  // bottom side
+            q[i] = q[i-N];
+            break;
         }
     }
-    // for (var i=0; i<N+2; i++) {
-    //     x[xy2i(  0,  i)] = 0;
-    //     x[xy2i(N+1,  i)] = 0;
-    //     x[xy2i(  i,  0)] = 0;
-    //     x[xy2i(  i,N+1)] = 0;
-    // }
-}
+};
 
-sim.vStep = function(u, v, u0, v0, visc, dt) {
-    var tmp;
-    sim.addSource(u, u0, dt);
-    sim.addSource(v, v0, dt);
-    tmp=u; u=u0; u0=tmp;
-    tmp=v; v=v0; v0=tmp;
-    sim.diffuse(sim.N, 1, u, u0, visc, dt);
-    sim.diffuse(sim.N, 2, v, v0, visc, dt);
-    sim.project(sim.N, u, v, u0, v0);
-    tmp=u; u=u0; u0=tmp;
-    tmp=v; v=v0; v0=tmp;
-    sim.advect(sim.N, 1, u, u0, u0, v0, dt);
-    sim.advect(sim.N, 2, v, v0, u0, v0, dt);
-    sim.project(sim.N, u, v, u0, v0);
-}
+sim.vStep = function(u1, v1, u0, v0, ubnd, vbnd, visc, dt) {
+    sim.addSource(u1, u0, dt);
+    sim.addSource(v1, v0, dt);
+    sim.diffuse(u0, u1, ubnd, visc, dt);
+    sim.diffuse(v0, v1, vbnd, visc, dt);
+    sim.project(u0, v0, u1, v1, ubnd, vbnd);
+    sim.advect(u1, u0, u0, v0, ubnd, dt);
+    sim.advect(v1, v0, u0, v0, vbnd, dt);
+    sim.project(u1, v1, u0, v0, ubnd, vbnd);
+};
 
-sim.qStep = function(x2, x1, u, v, kDiff, dt) {
-    var tmp;
-    sim.addSource(x2, x1, dt);
-    sim.diffuse(sim.N, 0, x1, x2, kDiff, dt);
-    sim.advect(sim.N, 0, x2, x1, u, v, dt);
-}
+sim.qStep = function(q1, q0, u, v, qbnd, kDiff, dt) {
+    sim.addSource(q1, q0, dt);
+    sim.diffuse(q0, q1, qbnd, kDiff, dt);
+    sim.advect(q1, q0, u, v, qbnd, dt);
+};
 
 sim.qDraw = function(ctx, q, scale) {
     var N = sim.N;
@@ -182,7 +211,7 @@ sim.qDraw = function(ctx, q, scale) {
         for (var i=0; i<N; i++) {
             var ind = 4*(j*N + i);
             var qInd = xy2i(i,j);
-            if (sim.bnd[qInd] > 0) {
+            if (sim.qbnd[qInd] >= 0) {
                 imgData.data[ind  ] = 255;
                 imgData.data[ind+1] = 255;
                 imgData.data[ind+2] = 255;
@@ -196,7 +225,7 @@ sim.qDraw = function(ctx, q, scale) {
         }
     }
     ctx.putImageData(imgData,0,0);
-}
+};
 
 sim.setFields = function(q0, u0, v0) {
     q0.set(sim.zeros);
@@ -214,25 +243,37 @@ sim.setFields = function(q0, u0, v0) {
             v0[ind] = userInput.v;
             break;
         case 1:
-            sim.bnd[ind] = 1;
+            sim.ubnd[ind] = 0;
+            sim.vbnd[ind] = 0;
+            sim.qbnd[ind] = 0;
             break;
+        case 2:
+            sim.forces.push([ind,
+                             sim.N*(sim.forceXEl.value||0),
+                             sim.N*(sim.forceYEl.value||0)]);
         }
     }
-}
+
+    for (var i=0; i<sim.forces.length; i++) {
+        var force = sim.forces[i];
+        u0[force[0]] = force[1];
+        v0[force[0]] = force[2];
+    }
+};
 
 sim.tick = function(t) {
     var dt = 0.001*(t - sim.lastT);
     sim.lastT = t;
 
     sim.setFields(sim.q0, sim.u0, sim.v0);
-    sim.vStep(sim.u, sim.v, sim.u0, sim.v0, sim.viscosity, dt);
-    sim.qStep(sim.q, sim.q0, sim.u, sim.v, sim.diffusion, dt);
-    sim.qDraw(sim.ctx, sim.q, 100);
+    sim.vStep(sim.u1, sim.v1, sim.u0, sim.v0, sim.ubnd, sim.vbnd, sim.viscosity, dt);
+    sim.qStep(sim.q1, sim.q0, sim.u1, sim.v1, sim.qbnd, sim.diffusion, dt);
+    sim.qDraw(sim.ctx, sim.q1, 100);
 
     sim.fps = 0.02/dt + 0.98*sim.fps;
     sim.fpsEl.textContent = sim.fps |0;
     sim.animationFrameId = requestAnimationFrame(sim.tick);
-}
+};
 
 sim.run = function() {
     sim.pause();
@@ -240,25 +281,29 @@ sim.run = function() {
         sim.lastT = t;
         sim.animationFrameId = requestAnimationFrame(sim.tick);
     });
-}
+};
 
 sim.pause = function() {
     cancelAnimationFrame(sim.animationFrameId);
-}
+};
 
 sim.setup = function() {
     sim.N = 64;
     sim.size = sim.N*sim.N;
 
-    ["u","u0","v","v0","q","q0","zeros"].forEach(function(field) {
-        sim[field] = new sim.FloatArray(sim.size);
-    })
-    sim.bnd = new Int8Array(sim.size);
-    sim.canvas = document.querySelector("#canvas");
+    ["u1","u0","v1","v0","q1","q0","zeros"].forEach(function(field) {
+        sim[field] = new Float64Array(sim.size);
+    });
+    ["ubnd", "vbnd", "qbnd"].forEach(function(field) {
+        sim[field] = new Int8Array(sim.size);
+    });
+    sim.canvas = document.querySelector("#stage");
     sim.ctx = sim.canvas.getContext("2d",{alpha:false});
     sim.canvas.width = sim.canvas.height = sim.N;
     sim.fpsEl = document.querySelector("#fps");
-    sim.resetBoundary();
+    sim.forceXEl = $("#force-x-input");
+    sim.forceYEl = $("#force-y-input");
+    sim.resetBoundaries();
 
     function switchInputType(t) {
         sim.userInput.type = t;
@@ -272,9 +317,14 @@ sim.setup = function() {
         userInput.v = 40*(y - userInput.y);
         userInput.x = x;
         userInput.y = y;
-        userInput.i = clamp((x*sim.N/e.target.offsetWidth |0) + 1, 1, sim.N);
-        userInput.j = clamp((y*sim.N/e.target.offsetHeight |0) + 1 ,1, sim.N);
+        userInput.i = clamp(x*sim.N/e.target.offsetWidth |0, 1, sim.N);
+        userInput.j = clamp(y*sim.N/e.target.offsetHeight |0 ,1, sim.N);
     }
+
+    function $(s) {
+        return document.querySelector(s);
+    }
+
     sim.canvas.addEventListener("mousedown", function(e) {
         sim.userInput.active = true;
         getInputXY(e);
@@ -288,18 +338,54 @@ sim.setup = function() {
     window.addEventListener("mouseup", function(e) {
         sim.userInput.active = false;
     })
-    document.querySelector("#color-button").addEventListener("click", function(e) {
+    $("#color-button").addEventListener("click", function(e) {
         switchInputType(0);
     });
-    document.querySelector("#wall-button").addEventListener("click", function(e) {
+    $("#wall-button").addEventListener("click", function(e) {
         switchInputType(1);
     });
-    document.querySelector("#wall-clear-button").addEventListener("click", sim.resetBoundary);
-    document.querySelector("#run-button").addEventListener("click", sim.run);
-    document.querySelector("#pause-button").addEventListener("click", sim.pause);
+    $("#force-button").addEventListener("click", function(e) {
+        switchInputType(2);
+    });
+    $("#forceremove-button").addEventListener("click", function(e) {
+        sim.forces.length = 0;
+    });
+    $("#numiter-input").addEventListener("change", function(e) {
+        var val = (+this.value |0) || 0;
+        sim.numIter = val;
+        this.value = val;
+    });
+    $("#viscosity-input").addEventListener("change", function(e) {
+        var val = +this.value || 0;
+        sim.viscosity = val;
+        this.value = val;
+    });
+    $("#diffusion-input").addEventListener("change", function(e) {
+        var val = +this.value || 0;
+        sim.diffusion = val;
+        this.value = val;
+    });
+    $("#color-clear-button").addEventListener("click", function(e) {
+        sim.q1.set(sim.zeros);
+    });
+    $("#wall-clear-button").addEventListener("click", function(e) {
+        sim.resetBoundaries();
+    });
+    $("#boxbnd-button").addEventListener("click", function(e) {
+        sim.bndType = 0;
+        sim.resetBoundaries();
+    });
+    $("#tunnelbnd-button").addEventListener("click", function(e) {
+        sim.bndType = 1;
+        sim.resetBoundaries();
+    });
+    $("#run-button").addEventListener("click", sim.run);
+    $("#pause-button").addEventListener("click", sim.pause);
     sim.run();
-}
+};
 
 sim.setup();
 
-})();
+exports.sim = sim;
+
+})(window);
